d5d2f1d3191f3f832eaa141ee1ac9263a1023425
estarli-var <lucordobaga@est.utn.ac.cr>
2026-07-24 22:56:42 -0600

    se hace la clase conometro
    se le hace el un contador que inicia desde cero y se usa lo que envio el profe al grupo

---
+++ src/conometros/Conometro.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conometros;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author estar
+ */
+public class Conometro {
+   private Timer timer;
+   private int segundos;
+   private int minutos;
+   private boolean corriendo;
+   
+   public Conometro() {
+        this.segundos = 0;
+        this.minutos = 0;
+        this.corriendo = false;
+   
+   this.timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contador();
+            }
+        });
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public boolean isCorriendo() {
+        return corriendo;
+    }
+
+    public void setCorriendo(boolean corriendo) {
+        this.corriendo = corriendo;
+        
+    }
+  
+    
+   public void contador(){
+   segundos++;
+   if(segundos>=60){
+   segundos=0;
+   minutos++;
+   }
+   }
+   public void iniciar() {
+        if (!corriendo) {
+            corriendo = true;
+            timer.start();
+        }
+    }
+   public void detener(){
+       if (corriendo) {
+        corriendo=false;
+        timer.stop();
+       }
+   }
+   
+   public void reiniciar(){
+   detener();
+   segundos=0;
+   minutos=0;
+   corriendo=false;
+   }
+   
+}
