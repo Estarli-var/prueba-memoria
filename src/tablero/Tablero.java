2773b28daa644b90fa0b62b690a251dfd35f0a3c
Wesley Ruiz <wruizm@est.utn.ac.cr>
2026-07-29 07:46:19 -0600

    mejoras de codigo
    Se realizan cambios en el codigo

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -12,46 +12,46 @@ import NivelDificultad.Dificultad;
  * @author wesrm
  */
 public class Tablero {
-    
+
     private Carta[][] cartas;
     private Dificultad dificultad;
     private String[] listaUsada = {};
-    
+
     public Carta[][] getCartas() {
         return cartas;
     }
-    
+
     public void setCartas(Carta[][] cartas) {
         this.cartas = cartas;
     }
-    
+
     @Override
     public String toString() {
         return "Tablero{" + "cartas=" + cartas + '}';
     }
-    
+
     public Dificultad getDificultad() {
         return dificultad;
     }
-    
+
     public String[] getListaUsada() {
         return listaUsada;
     }
-    
+
     public void setListaUsada(String[] listaUsada) {
         this.listaUsada = listaUsada;
     }
-    
+
     public void setDificultad(Dificultad dificultad) {
         this.dificultad = dificultad;
     }
-    
-    public void incializarTablero() {
-        for (int i = 0; i < (dificultad.getDifiMedidas() * dificultad.getDifiMedidas()); i++) {
+
+    public void inicializarTablero() {
+        for (int i = 0; i < cartas.length * cartas[0].length; i++) {
             boolean indicador = true;
             String[] vecTemp = new String[listaUsada.length + 1];
-            int val1 = (int) (Math.random() * dificultad.getDifiMedidas());
-            int val2 = (int) (Math.random() * dificultad.getDifiMedidas());
+            int val1 = (int) (Math.random() * cartas.length);
+            int val2 = (int) (Math.random() * cartas[0].length);
             String generado = val1 + "-" + val2;
             for (int j = 0; j < listaUsada.length; j++) {
                 if (listaUsada[j].equals(generado)) {
@@ -67,19 +67,20 @@ public class Tablero {
             }
         }
     }
-    
+
     public Tablero(Dificultad dificultad) {
         this.dificultad = dificultad;
-        this.cartas = new Carta[this.dificultad.getDifiMedidas()][this.dificultad.getDifiMedidas()];
-        this.incializarTablero();
+        int filas = 0;
+        int columnas = 0;
+        this.inicializarTablero();
         this.generarCartas();
     }
-    
+
     public void generarCartas() {
         int numeroCarta = 1;
         System.out.println(listaUsada.length);
         for (int i = 0; i < listaUsada.length; i++) {
-            
+
             for (int j = 0; j < 2; j++) {
                 System.out.println(i);
                 System.out.println(listaUsada[i]);
@@ -87,14 +88,14 @@ public class Tablero {
                 cartas[Integer.valueOf(valores[0])][Integer.valueOf(valores[1])] = new Carta(("carta_" + numeroCarta), ("carta" + numeroCarta));
                 if (j == 0) {
                     i++;
-                    
+
                 }
             }
             numeroCarta++;
-            
+
         }
     }
-    
+
     public boolean verificarCartas(Carta carta1, Carta carta2) {
         if (carta1.getNombre().equals(carta2.getNombre())) {
             carta1.setEstado(true);
@@ -104,11 +105,11 @@ public class Tablero {
             carta1.setEstado(true);
             carta2.setEstado(true);
             return false;
-            
+
         }
-        
+
     }
-    
+
     public boolean validarTodaslasCartas() {
         for (int i = 0; i < dificultad.getDifiMedidas(); i++) {
             for (int j = 0; j < dificultad.getDifiMedidas(); j++) {
@@ -119,13 +120,13 @@ public class Tablero {
         }
         return true;
     }
-    
+
     public void mostrarCarta(Carta carta) {
         carta.setEstado(true);
     }
-    
+
     public void ocultarCarta(Carta carta) {
         carta.setEstado(false);
     }
-    
+
 }
